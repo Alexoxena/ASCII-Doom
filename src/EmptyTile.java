1462d0b7801d0f5398621107d130641a671068a0
Von.Trotha.Alexandra <Von.Trotha.Alexandra@DESKTOP-FKR6M18.2631.skool.net>
2019-02-22 09:01:28 +0100

    tiles & position

--- src/EmptyTile.java
+++ src/EmptyTile.java
@@ -1,6 +1,11 @@
 
 public class EmptyTile extends GameTile{
 
+	public EmptyTile(Position position) {
+		super(position);
+		// TODO Auto-generated constructor stub
+	}
+
 	public String toString(){
 		
 		return " ";
--- src/GameTile.java
+++ src/GameTile.java
@@ -3,6 +3,14 @@ public class GameTile {
 	
 	private Position position;
 	
+	public GameTile(Position position){
+		this.position=position;
+	
+	}
+	
+	public GameTile ( int x, int y){
+		
+	}
 	public String toString(){
 		return null;
 	}
