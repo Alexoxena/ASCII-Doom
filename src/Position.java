aaba9043b3033a19d63f28c530c5fb122f23e3b5
Von.Trotha.Alexandra <Von.Trotha.Alexandra@DESKTOP-FKR6M18.2631.skool.net>
2019-02-22 08:19:30 +0100

    2nd commit

---
+++ src/Position.java
@@ -0,0 +1,26 @@
+
+public class Position {
+	
+	private int x;
+	private int y;
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	public void setX (int x){
+		this.x  = x;
+	}
+	
+	public void setY (int x){
+		this.y = y;
+	}
+	
+	public boolean equals(int x, int y){
+		
+	}
+}
